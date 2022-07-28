66f170f822c53ddbcebb171bb212ccdfb82040dd
LeeMong <izpzgi2019@gmail.com>
2022-07-28 18:10:30 +0900

    validation 적용

--- src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
+++ src/main/java/com/fastcampus/programming/dmaker/controller/DMakerController.java
@@ -1,18 +1,23 @@
 package com.fastcampus.programming.dmaker.controller;
 
 
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.service.DMakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * @author L
  */
+
 @Slf4j
 @RestController // @Controller +  @ResponseBody(VO객체를 JSON으로 바꿔서 HTTP body에 담는 스프링 어노테이션)
 @RequiredArgsConstructor
@@ -20,21 +25,23 @@ public class DMakerController {
 
     private final DMakerService dMakerService;
 
+
     @GetMapping("/developers")
     public List<String> getAllDevelopers() {
-      // GET /developers HTTP/1.1
+        // GET /developers HTTP/1.1
         log.info("GET /delvelopers HTTP/1.1");
 
         return Arrays.asList("L", "mong", "Olaf");
     }
 
-    @GetMapping("/create-developers")
-    public List<String> createDevelopers() {
+    @PostMapping("/create-developers")
+    public CreateDeveloper.Response createDevelopers(@Valid @RequestBody CreateDeveloper.Request request) {
         // GET /developers HTTP/1.1
-        log.info("GET /create-developers HTTP/1.1");
+//      log.info("GET /create-developers HTTP/1.1");
+        log.info("request : {}", request);
 
-        dMakerService.createDeveloper();
+        return dMakerService.createDeveloper(request);
 
-        return List.of("Olaf");
+
     }
 }
--- src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
+++ src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
@@ -1,12 +1,19 @@
 package com.fastcampus.programming.dmaker.service;
 
+import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.entity.Developer;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
 import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
 import com.fastcampus.programming.dmaker.type.DeveloperLevel;
 import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.fastcampus.programming.dmaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static com.fastcampus.programming.dmaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEAR_NOT_MATCHED;
 
 /**
  * @author L
@@ -16,16 +23,50 @@ import org.springframework.transaction.annotation.Transactional;
 public class DMakerService {
     private final DeveloperRepository developerRepository;
 
+
+    // ACID
+    // Atomicity(원자성)
+    // Consistency(일관성)
+    // isolation(독립성)
+    // Durability(영속성)
     @Transactional
-    public void createDeveloper() {
+    public CreateDeveloper.Response createDeveloper(CreateDeveloper.Request request) {
+        validateCreateDeveloperRequest(request);
         Developer developer = Developer.builder()
-                .developerLevel(DeveloperLevel.JUNIOR)
-                .developerSkillType(DeveloperSkillType.FRONT_END)
-                .experienceYears(2)
-                .name("mong")
-                .age(5)
+                .developerLevel(request.getDeveloperLevel())
+                .developerSkillType(request.getDeveloperSkillType())
+                .experienceYears(request.getExperienceYears())
+                .memberId(request.getMemberId())
+                .name(request.getName())
+                .age(request.getAge())
                 .build();
 
         developerRepository.save(developer);
+        return CreateDeveloper.Response.fromEntity(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+        //business validation
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer experienceYears = request.getExperienceYears();
+        if (developerLevel == DeveloperLevel.SENIOR && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEAR_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNGNIOR
+                && (experienceYears < 4 || experienceYears > 10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEAR_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNGNIOR && experienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEAR_NOT_MATCHED);
+        }
+
+        developerRepository.findByMemberId(request.getMemberId())
+                .ifPresent((developer ->{
+                    throw new DMakerException(DUPLICATED_MEMBER_ID);
+                }));
+
+
     }
 }
